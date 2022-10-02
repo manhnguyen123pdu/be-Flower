6f19de34e08ca918f6efe6b394685837784e7aef
John Doe <manhnguyen.tla@gmail.com>
2022-10-02 13:18:44 +0700

    s

--- JAVASCRIP/jshomepage/jshome.js
+++ JAVASCRIP/jshomepage/jshome.js
@@ -1,35 +1,37 @@
-function shownav() {
-    document.querySelector('.header__overlay').classList.add('shownav')
-}
-function hindNav() {
-    document.querySelector('.header__overlay').classList.remove('shownav')
-
-}
-
-//  đổi mầu nav
-addEventListener('scroll', function () {
-    if (pageYOffset > 180) {
-        document.querySelector('.header__content').style.backgroundColor = ' #0d302ef0'
+document.addEventListener('DOMContentLoaded',function(){
+    function shownav() {
+        document.querySelector('.header__overlay').classList.add('shownav')
     }
-    else {
-        document.querySelector('.header__content').style.backgroundColor = 'transparent'
+    function hindNav() {
+        document.querySelector('.header__overlay').classList.remove('shownav')
+    
     }
-})
-
-// RENDER PRODUCT
-function rederProduct() {
-    let JsonListProduct = JSON.parse(window.localStorage.getItem('localListProduct'));
-    for (i = 0; i < JsonListProduct.length; i++) {
-        let content = document.createElement('div')
-        content.innerHTML= `
-        <div class="item">
-            <img src="${JsonListProduct[i].image}"
-            alt="">
-            <h4>${JsonListProduct[i].nameProduct}</h4>
-            <span>$${Number(JsonListProduct[i].price.substr(0,1)).toFixed(2)} - $${Number(JsonListProduct[i].price.substr(2,2)).toFixed(2)}  </span>
-         </div> `
-         document.querySelector('.owl-carousel').appendChild(content);
+    
+    //  đổi mầu nav
+    addEventListener('scroll', function () {
+        if (pageYOffset > 180) {
+            document.querySelector('.header__content').style.backgroundColor = ' #0d302ef0'
+        }
+        else {
+            document.querySelector('.header__content').style.backgroundColor = 'transparent'
+        }
+    })
+    
+    // RENDER PRODUCT
+    function rederProduct() {
+        let JsonListProduct = JSON.parse(window.localStorage.getItem('localListProduct'));
+        for (i = 0; i < JsonListProduct.length; i++) {
+            let content = document.createElement('div')
+            content.innerHTML= `
+            <div class="item">
+                <img src="${JsonListProduct[i].image}"
+                alt="">
+                <h4>${JsonListProduct[i].nameProduct}</h4>
+                <span>$${Number(JsonListProduct[i].price.substr(0,1)).toFixed(2)} - $${Number(JsonListProduct[i].price.substr(2,2)).toFixed(2)}  </span>
+             </div> `
+             document.querySelector('.owl-carousel').appendChild(content);
+        }
     }
-}
-
-rederProduct()
+    
+    rederProduct()
+},false)
